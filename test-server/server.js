504cccdaf819cb73f6e83c67f03fbe35efb198fa
Suyog B <mail@suyogb.com>
2025-07-23 01:24:02 -0700

    now serving configurator

--- test-server/server.js
+++ test-server/server.js
@@ -3,8 +3,9 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+app.use('/', express.static(path.join(__dirname, '../', 'configurator')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'test.html'));
+  res.sendFile(path.join(__dirname, '../', 'configurator', 'welcome.html'));
 });
 
 app.listen(PORT, () => {
